90f3881e486038eb4649d7b75f441759e9a0dcd5
M. Serdar Karaman <m.serdar.karaman@gmail.com>
2022-11-23 00:54:13 +0300

    \r\n

--- kernel.c
+++ kernel.c
@@ -1,18 +1,19 @@
 #include "print.h"
 #include "uart.h"
+
 void Serd_OS_main()
 {
     uart_init();
-    uart_writeArray("UART is initialized!\n");
+    uart_writeArray("UART is initialized!\r\n");
 
     // TODO initilaze other peripherals
 
-    uart_writeArray("Initialization complete!\n");
+    uart_writeArray("Initialization complete!\r\n");
 
     int num = 10;
-    printk("Num= %d\n", num);
-    printk("Hex= %x\n", num);
-    printk("Str= %s\n", "num");
+    printk("Num= %d\r\n",num);
+    printk("Hex= %x\r\n",num);
+    printk("Str= %s\r\n","num");
 
     while (1)
     {
@@ -20,7 +21,7 @@ void Serd_OS_main()
         uart_writeChar(c);
         if (c == 'A')
         {
-            uart_writeArray("\nA pressed\n");
+            uart_writeArray("\r\nA is pressed\r\n");
         }
     }
 }
