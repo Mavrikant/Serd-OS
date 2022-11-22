64ff9946cfb788dbb90b5536c5725e05bd918331
Clang Format <clang-format@karaman.dev>
2022-11-22 21:07:33 +0000

    apply clang-format

--- kernel.c
+++ kernel.c
@@ -1,19 +1,18 @@
 #include "uart.h"
 
-void Serd_OS_main()
-{
-    uart_init();
-    uart_writeArray("UART is initialized!\n");
+void Serd_OS_main() {
+  uart_init();
+  uart_writeArray("UART is initialized!\n");
 
-    //TODO initilaze other peripherals
+  // TODO initilaze other peripherals
 
-    uart_writeArray("Initialization complete!\n");
+  uart_writeArray("Initialization complete!\n");
 
-    while(1) {
-        char c = uart_readChar();
-        uart_writeChar(c);
-        if(c == 'A'){
-            uart_writeArray("\nA pressed\n");
-        }
+  while (1) {
+    char c = uart_readChar();
+    uart_writeChar(c);
+    if (c == 'A') {
+      uart_writeArray("\nA pressed\n");
     }
+  }
 }
--- mbox.c
+++ mbox.c
@@ -1,30 +1,33 @@
 #include "registers.h"
 
 /* mailbox message buffer */
-volatile unsigned int  __attribute__((aligned(16))) mbox[36];
+volatile unsigned int __attribute__((aligned(16))) mbox[36];
 
-#define MBOX_RESPONSE   0x80000000
-#define MBOX_FULL       0x80000000
-#define MBOX_EMPTY      0x40000000
+#define MBOX_RESPONSE 0x80000000
+#define MBOX_FULL 0x80000000
+#define MBOX_EMPTY 0x40000000
 
 /**
  * Make a mailbox call. Returns 0 on failure, non-zero on success
  */
-int mbox_call(unsigned char ch)
-{
-    unsigned int r = (((unsigned int)((unsigned long)&mbox)&~0xF) | (ch&0xF));
-    /* wait until we can write to the mailbox */
-    do{asm volatile("nop");}while(*MBOX_STATUS & MBOX_FULL);
-    /* write the address of our message to the mailbox with channel identifier */
-    *MBOX_WRITE = r;
-    /* now wait for the response */
-    while(1) {
-        /* is there a response? */
-        do{asm volatile("nop");}while(*MBOX_STATUS & MBOX_EMPTY);
-        /* is it a response to our message? */
-        if(r == *MBOX_READ)
-            /* is it a valid successful response? */
-            return mbox[1]==MBOX_RESPONSE;
-    }
-    return 0;
+int mbox_call(unsigned char ch) {
+  unsigned int r = (((unsigned int)((unsigned long)&mbox) & ~0xF) | (ch & 0xF));
+  /* wait until we can write to the mailbox */
+  do {
+    asm volatile("nop");
+  } while (*MBOX_STATUS & MBOX_FULL);
+  /* write the address of our message to the mailbox with channel identifier */
+  *MBOX_WRITE = r;
+  /* now wait for the response */
+  while (1) {
+    /* is there a response? */
+    do {
+      asm volatile("nop");
+    } while (*MBOX_STATUS & MBOX_EMPTY);
+    /* is it a response to our message? */
+    if (r == *MBOX_READ)
+      /* is it a valid successful response? */
+      return mbox[1] == MBOX_RESPONSE;
+  }
+  return 0;
 }
