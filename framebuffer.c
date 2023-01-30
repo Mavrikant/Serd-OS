497b8ee8e4d0fac76a22774eff082488e604b718
Clang Format <clang-format@karaman.dev>
2023-01-30 03:56:37 +0000

    apply clang-format

--- framebuffer.c
+++ framebuffer.c
@@ -1,18 +1,19 @@
+#include "hacettepe_logo.h"
 #include "mbox.h"
 #include "serd-os_logo.h"
-#include "hacettepe_logo.h"
 #include "terminal.h"
 #include "uart.h"
 
 unsigned int width, height, pitch, isrgb; /* dimensions and channel order */
 unsigned char *fb;                        /* raw frame buffer address */
 
-#define HEADER_PIXEL(data,pixel) {\
-pixel[0] = (((data[0] - 33) << 2) | ((data[1] - 33) >> 4)); \
-pixel[1] = ((((data[1] - 33) & 0xF) << 4) | ((data[2] - 33) >> 2)); \
-pixel[2] = ((((data[2] - 33) & 0x3) << 6) | ((data[3] - 33))); \
-data += 4; \
-}
+#define HEADER_PIXEL(data, pixel)                                                                                                                                                                                                              \
+    {                                                                                                                                                                                                                                          \
+        pixel[0] = (((data[0] - 33) << 2) | ((data[1] - 33) >> 4));                                                                                                                                                                            \
+        pixel[1] = ((((data[1] - 33) & 0xF) << 4) | ((data[2] - 33) >> 2));                                                                                                                                                                    \
+        pixel[2] = ((((data[2] - 33) & 0x3) << 6) | ((data[3] - 33)));                                                                                                                                                                         \
+        data += 4;                                                                                                                                                                                                                             \
+    }
 
 /**
  * Set screen resolution to 800x480
